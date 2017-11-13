9e0c25aaca2b282eb1322d82b122029d5e5a11e1
Jubing Chen <papalagi.chen@gmail.com>
2017-11-13 08:51:04 +0800

    Update decision tree

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -24,8 +24,23 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+if __name__ == '__main__':
+    from sklearn.tree import DecisionTreeClassifier
 
+    classifier = DecisionTreeClassifier(min_samples_split=40)
 
+    t0 = time()
+    classifier.fit(features_train, labels_train)
+    print "training time: ", round(time() - t0, 3), "s"
+
+    t1 = time()
+    classifier.predict(features_test)
+    print "predicting time: ", round(time() - t1, 3), "s"
+
+    t2 = time()
+    score = classifier.score(features_test, labels_test)
+    print "scoring time: ", round(time() - t2, 3), "s"
+    print "score: ", score
 #########################################################
 
 
